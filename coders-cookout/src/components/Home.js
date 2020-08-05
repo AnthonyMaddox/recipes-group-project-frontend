604e706dfe8b86acf763cd46c3e1d29038b7f573
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-08-05 13:47:59 -0400

    trying to merge master

--- coders-cookout/src/components/Home.js
+++ coders-cookout/src/components/Home.js
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
 import React from "react";
 import "../App.css";
-
-function Home() {
-  return (
-    <div>
-      <h3>Recipe of the Day</h3>
-    </div>
-=======
-import React from 'react';
-import "../App.css"
-import Image from './Images/Image'
-import './Images/Image.css'
+import Image from "./Images/Image";
+import "./Images/Image.css";
 
 function Home() {
   return (
     <div className="homeDiv">
-      <Image src="https://images.unsplash.com/photo-1568901839119-631418a3910d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
+      <Image src="https://images.unsplash.com/photo-1568901839119-631418a3910d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" />
     </div>
-    
->>>>>>> 188846b0559a4e993df9b2b443687b308ce8d826
   );
 }
 

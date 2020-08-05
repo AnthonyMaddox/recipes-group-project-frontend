4d629203ec0c6943bab0dfdba67512cb332ca829
HemaO <hema.omprakash@gmail.com>
2020-08-05 09:20:36 -0400

    added components to storybook

--- coders-cookout/src/components/Buttons/Button.stories.js
+++ coders-cookout/src/components/Buttons/Button.stories.js
@@ -2,6 +2,14 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Button from "./Button";
 
+const style = {
+  backgroundColor: '#E27878',
+  color: 'white',
+  borderRadius: '2px',
+  padding: '5px 25px',
+  border: '#E27878'
+}
+
 storiesOf("Button", module).add("Primary", () => (
-    <Button label="Do Something" />
-  ))
+    <Button label="Get Recipe" style={style}/>
+))
--- coders-cookout/src/components/Home.js
+++ coders-cookout/src/components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import "../App.css"
+import Image from './Images/Image'
+import './Images/Image.css'
 
 function Home() {
   return (
-    <div className="homeDiv"></div>
-  
+    <div className="homeDiv">
+      <Image src="https://images.unsplash.com/photo-1568901839119-631418a3910d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"/>
+    </div>
+    
   );
 }
 

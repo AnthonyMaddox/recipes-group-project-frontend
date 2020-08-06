77810afdde77ff83d10296e0ba6831f86b8f57e8
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-08-06 08:35:29 -0400

    added elete and update forms to match routes

--- coders-cookout/src/components/Forms/UpdateForm.js
+++ coders-cookout/src/components/Forms/UpdateForm.js
@@ -7,7 +7,7 @@ import "./Form.css";
 //need to use e.target.value and set state
 // convert to a class!!! set state and include something to hold the value of input.value
 let classList = [];
-class CreateForm extends Component {
+class UpdateForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -19,7 +19,17 @@ class CreateForm extends Component {
   render() {
     return (
       <div>
-        <h4 className="createFormTitle">Add A New Recipe!</h4>
+        <h4 className="createFormTitle">Update A Recipe By ID</h4>
+        <div className="idField">
+          <p>ID:</p>
+          <input
+            id="createTitle"
+            className={classList.join(" ")}
+            type="text"
+            value={this.state.value}
+            placeholder={this.state.placeholder}
+          />
+        </div>
         <div id="createForm" className="createForm">
           <div className="addRecipeDiv">
             <div>
@@ -46,8 +56,6 @@ class CreateForm extends Component {
                 id="textField"
                 className={classList.join(" ")}
                 type="text"
-                value={this.state.value}
-                placeholder={this.state.placeholder}
               />
             </div>
             <div>
@@ -56,38 +64,12 @@ class CreateForm extends Component {
                 id="textField"
                 className={classList.join(" ")}
                 type="text"
-                value={this.state.value}
-                placeholder={this.state.placeholder}
               />
             </div>
           </div>
           <div className="ingedientsDiv">
             <div>
-              <p className="subtitle">Add Ingredient</p>
-              <p>Ingredient:</p>
-              <input
-                id="addIngredient"
-                className={classList.join(" ")}
-                type="text"
-                value={this.state.value}
-                placeholder={this.state.placeholder}
-              />
-            </div>
-            <div>
-              <p>Amount:</p>
-              <input
-                id="addAmount"
-                className={classList.join(" ")}
-                type="text"
-                value={this.state.value}
-                placeholder={this.state.placeholder}
-              />
-            </div>
-            <div>
-              <button>Add Ingredient</button>
-            </div>
-            <div>
-              <p className="subtitle">Delete Ingredient</p>
+            <p className="subtitle">Add Ingredient</p>
               <p>Ingredient:</p>
               <input
                 id="addIngredient"
@@ -105,23 +87,36 @@ class CreateForm extends Component {
             </div>
             <div>
               <button>Add Ingredient</button>
+            </div>
+            <div>
+            <p className="subtitle">Delete Ingredient</p>
+              <p>Ingredient:</p>
+              <input
+                id="addIngredient"
+                className={classList.join(" ")}
+                type="text"
+              />
+            </div>
+            <div>
+              <p>Amount:</p>
+              <input
+                id="addAmount"
+                className={classList.join(" ")}
+                type="text"
+              />
+            </div>
+            <div>
+              <button>Delete Ingredient</button>
             </div>
           </div>
         </div>
         <div className="bigButton">
           <br></br>
-          <button>Create Recipe</button>
+          <button>Update Recipe</button>
         </div>
       </div>
     );
   }
 }
 
-export default CreateForm;
-//  if (props.large) {
-//    classList.push("large");
-// }
-// if (props.small) {
-//   classList.push("small");
-// }
-//value={this.state.value} onChange={this.handleChange}
+export default UpdateForm;

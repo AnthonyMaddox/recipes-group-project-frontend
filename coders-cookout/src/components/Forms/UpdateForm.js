fb0fff1544ebf4efcb76b8130ba8e5452a252cbd
Naira_Reyes <gab1ana1@gmail.com>
2020-08-07 09:33:57 -0400

    Accepted changes

--- coders-cookout/src/components/Forms/UpdateForm.js
+++ coders-cookout/src/components/Forms/UpdateForm.js
@@ -59,43 +59,6 @@ class UpdateForm extends Component {
   render() {
     return (
       <div className="createForm">
-<<<<<<< HEAD
-        <form onSubmit={this.handleSubmit}>
-          <h4 className="createFormTitle">Update Recipe By ID</h4>
-          <div id="createForm">
-            <div className="addRecipeDiv">
-              <div className="idField">
-                <p className="pId">ID:</p>
-                <input id="idRecipeInput" type="text" />
-              </div>
-              <div>
-                <p className="inputLabelUpdate">Title:</p>
-                <input id="updateTitle" type="text" />
-              </div>
-              <div>
-                <p className="inputLabelUpdate">Description:</p>
-                <textarea id="updateDescription" />
-              </div>
-              <div>
-                <p className="inputLabelUpdate">Instructions:</p>
-                <textarea id="updateInstructions" />
-              </div>
-              <div>
-                <p className="inputLabelUpdate">Image URL Link:</p>
-                <input
-                  id="updatePicture"
-                  className={classList.join(" ")}
-                  type="text"
-                />
-              </div>
-              <div>
-                <p className="inputLabelUpdate">Cook:</p>
-                <input
-                  id="updateCook"
-                  className={classList.join(" ")}
-                  type="text"
-                />
-=======
         <form onSubmit={this.handleSubmit} className="updateForm">
           <h4 className="createFormTitle">Update Recipe By ID</h4>
           <br></br>
@@ -124,13 +87,10 @@ class UpdateForm extends Component {
               <div>
                 <p className="inputLabel">Cook:</p>
                 <input id="createCook" type="text" />
->>>>>>> d73f47ddf070bdb89209c98e6b46d2f9e7479156
               </div>
             </div>
             <div className="bigButton">
               <br></br>
-<<<<<<< HEAD
-
               <button className="updateRecipeButton">Update Recipe</button>
             </div>
           </div>
@@ -138,38 +98,6 @@ class UpdateForm extends Component {
         {/* ingredients form */}
         <form className="ingredientUpdateForm" onSubmit={this.handleISubmit}>
           <div>
-            <h4 className="createFormTitle">Add Ingredient By Recipe ID</h4>
-            <div className="idField">
-              <p className="addId">ID:</p>
-              <input id="idIngredientInput" type="text" />
-            </div>
-          </div>
-          <div>
-            <p className="subtitle">Add Ingredient</p>
-            <p className="Label">Ingredient:</p>
-            <input id="ingredient" type="text" />
-=======
-              <button className="updateRecipeButton">Update Recipe</button>
-            </div>
->>>>>>> d73f47ddf070bdb89209c98e6b46d2f9e7479156
-          </div>
-        </form>
-        {/* ingredients form */}
-        <form className="ingredientUpdateForm" onSubmit={this.handleISubmit}>
-          <div>
-<<<<<<< HEAD
-            <p className="inputLabel">Amount:</p>
-            <input id="amount" type="text" />
-          </div>
-          <div>
-            <button className="ingredientButton">Add Ingredient</button>
-          </div>
-          <div>
-            <h4 className="deleteTitle">Delete Ingredient By Ingredient ID</h4>
-            <p className="subtitle">Delete Ingredient</p>
-            <p className="inputLabel">Ingredient:</p>
-            <input id="deleteIngredient" type="text" />
-=======
             <h4 className="createFormTitle">Add Ingredient By Recipe ID</h4>
             <br></br>
             <div className="idField">
@@ -181,7 +109,6 @@ class UpdateForm extends Component {
             <p className="subtitle">Add Ingredient</p>
             <p className="inputLabel">Ingredient:</p>
             <input id="updateIngredient" type="text" />
->>>>>>> d73f47ddf070bdb89209c98e6b46d2f9e7479156
           </div>
           <div>
             <p className="inputLabel">Amount:</p>
@@ -189,11 +116,7 @@ class UpdateForm extends Component {
           </div>
           <br></br>
           <div>
-<<<<<<< HEAD
-            <button className="ingDeleteButton">Delete Ingredient</button>
-=======
             <button className="formButton">Add Ingredient</button>
->>>>>>> d73f47ddf070bdb89209c98e6b46d2f9e7479156
           </div>
         </form>
       </div>

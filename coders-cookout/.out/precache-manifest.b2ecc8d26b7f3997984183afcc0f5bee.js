3a1d05b3fbfe24abe116d2b0095ffe6942764a77
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-08-10 08:22:18 -0400

    ran npm run build storybook

---
+++ coders-cookout/.out/precache-manifest.b2ecc8d26b7f3997984183afcc0f5bee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3373bdacd700d6f2b6d0fe475e72a089",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.8c09734ca62f78677052.bundle.js"
+  },
+  {
+    "url": "runtime~main.8c09734ca62f78677052.bundle.js"
+  },
+  {
+    "revision": "8547d086e702e44d330d",
+    "url": "static/css/main.b14fd1f5.chunk.css"
+  },
+  {
+    "url": "vendors~main.8c09734ca62f78677052.bundle.js"
+  },
+  {
+    "url": "vendors~main.8c09734ca62f78677052.bundle.js.LICENSE.txt"
+  }
+]);
